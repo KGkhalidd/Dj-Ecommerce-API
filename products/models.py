ab107b36c20433ec14850b5a1612613830fe6283
khaled gamal <khaledgamal1345@gmail.com>
2024-01-01 17:34:19 +0200

    created products app and added model Product

---
+++ products/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.TextChoices):
+    ELECTRONICS = 'Electronics'
+    CLOTHING = 'Clothing'
+    BOOKS = 'Books'
+    SPORTS = 'Sports'
+    HOME = 'Home'
+    OTHER = 'Other'
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, default='', blank = False)
+    description = models.TextField(max_length=1000 ,default='', blank = False)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
+    brand = models.CharField(max_length=200, default='', blank = False)
+    category = models.CharField(max_length=40, choices=Category.choices, default=Category.OTHER)
+    ratings = models.DecimalField( max_digits=5, decimal_places=2, default=0.00)
+    stock = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, null = True, on_delete=models.SET_NULL)
