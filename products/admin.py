9d4f9b4664d88f64a5622fb6a817d837a1013c71
khaled gamal <khaledgamal1345@gmail.com>
2024-01-01 18:44:35 +0200

    created serializer & view for listing products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'brand', 'price', 'stock', 'ratings', 'created')
+    list_filter = ('category', 'brand')
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_all_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
