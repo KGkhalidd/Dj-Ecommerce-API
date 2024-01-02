0ce2c28a94fbe3ff2eb6903be49e6422e5e3c193
khaled gamal <khaledgamal1345@gmail.com>
2024-01-02 20:52:37 +0200

    Implemented pagination for product list API endpoint

--- products/views.py
+++ products/views.py
@@ -4,12 +4,15 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 from .filters import ProductFilter
+from rest_framework.pagination import PageNumberPagination
 
 @api_view(['GET'])
 def product_list(request):
-    #products = Product.objects.all()
     filterset = ProductFilter(request.GET, queryset=Product.objects.all().order_by('id'))
-    serializer = ProductSerializer(filterset.qs, many=True)
+    paginator = PageNumberPagination()
+    paginator.page_size = 2
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+    serializer = ProductSerializer(queryset, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
