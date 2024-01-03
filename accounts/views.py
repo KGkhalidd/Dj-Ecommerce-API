b43f3106bfe72c330c926ea8462dacb94fc36f1e
khaled gamal <khaledgamal1345@gmail.com>
2024-01-03 06:18:45 +0200

    Implemented user registration endpoint with email uniqueness validation

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import SignUpSerializer, UserSerializer
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework import status
 
-# Create your views here.
+@api_view(['POST'])
+def register(request):
+    serializer = SignUpSerializer(data=request.data)
+    if serializer.is_valid():
+        if not User.objects.filter(email = serializer.validated_data['email']).exists():
+            user = User.objects.create_user(
+                username=serializer.validated_data['email'],
+                first_name = serializer.validated_data['first_name'],
+                last_name = serializer.validated_data['last_name'],
+                email = serializer.validated_data['email'],
+                password = serializer.validated_data['password']
+            )
+            return Response({'message': 'Account created successfully', 'user': UserSerializer(user).data}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('products.urls')),
+    path('api/', include('accounts.urls')),
 ]
 
 handler404 = 'utils.error_view.handler404'
