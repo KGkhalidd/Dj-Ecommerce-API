53fe81c71492e1603e67a5b03fe9b35a497d715f
khaled gamal <khaledgamal1345@gmail.com>
2024-01-04 08:28:47 +0200

    added url for endpoint delete review

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('products/<int:pk>/update', views.product_update, name='product-update'),
     path('products/<int:pk>/delete', views.product_delete, name='product-delete'),
     path('products/<int:pk>/reviews', views.review_create_or_update, name='review-create-or-update'),
+    path('products/<int:pk>/reviews/delete', views.review_delete, name='review-delete'),
 ]
