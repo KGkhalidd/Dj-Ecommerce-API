70a5ca96e7e4347927207b80971d23220781cdd8
khaled gamal <khaledgamal1345@gmail.com>
2024-01-04 07:33:39 +0200

    Added reviews field to ProductSerializer to include product reviews in serialized output

--- products/serializers.py
+++ products/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from .models import Product, Review
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'brand', 'category', 'ratings', 'stock', 'created', 'user', 'reviews']
