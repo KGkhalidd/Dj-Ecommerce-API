5fdb5cde9e2bdde9c72dc6494bf277fcb00f01f4
khaled gamal <khaledgamal1345@gmail.com>
2024-01-04 05:00:07 +0200

    Implemented ReviewSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Review
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
         fields = '__all__'
