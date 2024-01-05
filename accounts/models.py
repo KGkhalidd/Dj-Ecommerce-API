1fe511c383827bb221970776bead058e5bef99ee
khaled gamal <khaledgamal1345@gmail.com>
2024-01-05 03:13:50 +0200

    Added post_save signal to automatically create a Profile instance whenever a new User instance is created

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +10,9 @@ class Profile(models.Model):
     
     def __str__(self):
         return f"{self.user.username} Profile"
+    
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
