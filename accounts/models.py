67959b4e0ca1fde0b9161986af9df4380ee2d370
khaled gamal <khaledgamal1345@gmail.com>
2024-01-05 02:43:57 +0200

    created model profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    reset_password_token = models.CharField(max_length=100, blank=True, null=True)
+    reset_password_expires = models.DateTimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.user.username} Profile"
