6118e72af90cb5abc4deabe531a94f51e13b858d
khaled gamal <khaledgamal1345@gmail.com>
2024-01-05 05:50:01 +0200

    Added forgot_password and reset_password routes to Django URL configuration

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register', views.register , name='register'),
     path('user_info', views.user_info , name='user_info'),
     path('user_info/update', views.update_user , name='update'),
+    path('forgot_password', views.forgot_password , name='forgot_password'),
+    path('reset-password/<str:token>', views.reset_password , name='reset_password'),
 ]
