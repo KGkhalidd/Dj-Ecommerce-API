81f6d7ba4771e1811ce7c77206fc787c9290872e
khaled gamal <khaledgamal1345@gmail.com>
2024-01-06 08:08:28 +0200

    Implemented Order and OrderItem serializers with custom method for retrieving order items

---
+++ orders/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = serializers.SerializerMethodField(method_name='get_order_items', read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_order_items(self, obj):
+        order_items = obj.items.all()
+        serializer = OrderItemSerializer(order_items, many=True)
+        return serializer.data
