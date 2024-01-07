ca69778e61f51df3d974ad65f8d842a31677a822
khaled gamal <khaledgamal1345@gmail.com>
2024-01-07 04:04:57 +0200

    corrects the path

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import OrderView
+from .views import OrderView, OrderDetailView
 
 urlpatterns = [
-    path('orders/', OrderView.as_view(), name='orders'),
-    path('orders/<int:id>/', OrderView.as_view(), name='order-detail'),
+    path('orders', OrderView.as_view(), name='orders'),
+    path('orders/<int:id>', OrderDetailView.as_view(), name='order-detail'),
 ]
